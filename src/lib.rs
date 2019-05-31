3c1656b24ac43fc351b21db5fb268c43f0b9d804
zhangsoledad <787953403@qq.com>
2019-05-31 17:19:54 +0800

    ci: setup windows ci

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,15 @@
 mod binding;
 mod blake2b;
 
+#[cfg(test)]
+#[cfg(not(windows))]
+#[allow(clippy::all)]
+#[allow(
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    dead_code
+)]
+mod binding_layout_tests;
+
 pub use crate::blake2b::{blake2b, Blake2b, Blake2bBuilder};
