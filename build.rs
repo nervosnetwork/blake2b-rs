485c3a1f6784c30142bfdd9f8f33b2e348c165a2
zhangsoledad <787953403@qq.com>
2023-02-28 16:49:22 +0800

    feat: runtime binding generation

--- build.rs
+++ build.rs
@@ -3,11 +3,30 @@ fn main() {
     // defined since it has not features.
     let features = std::env::var("CARGO_CFG_TARGET_FEATURE").unwrap_or_default();
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let bindings_builder = bindgen::Builder::default().use_core().ctypes_prefix("cty");
+
     if features.contains("sse4.1") || features.contains("sse2") {
+        let bindings = bindings_builder
+            .header("BLAKE2/sse/blake2.h")
+            .generate()
+            .expect("unable to generate bindings");
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("unable to write bindings");
+
         cc::Build::new()
             .file("BLAKE2/sse/blake2b.c")
             .compile("libblake2b.a");
     } else {
+        let bindings = bindings_builder
+            .header("BLAKE2/ref/blake2.h")
+            .generate()
+            .expect("unable to generate bindings");
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("unable to write bindings");
+
         let mut build = cc::Build::new();
         if target_arch == "riscv64" {
             // Blake2b only requires a small part of libc, e.g., stdint.h for
